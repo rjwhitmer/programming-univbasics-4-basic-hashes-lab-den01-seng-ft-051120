fcc072b69bb62395c1ff8aabe8de016b399474df
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 21:35:59 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,23 @@
 def new_hash
-  # return an empty hash
+  new_hash = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash = {
+    :baseball_team => "reds"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+    :name => "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  hash = {
+    :id => number
+  }
 end
